45b25380a60a75ad9669ddca142b42c0ca16a455
Xusen Yin <yinxusen@gmail.com>
2015-08-12 11:11:13 +0800

    comments on sampler

--- src/main/scala/org/apache/spark/ml/tuning/bandit/ParamSampler.scala
+++ src/main/scala/org/apache/spark/ml/tuning/bandit/ParamSampler.scala
@@ -1,13 +1,37 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package org.apache.spark.ml.tuning.bandit
 
 import scala.util.Random
 
 /**
- * Created by panda on 8/10/15.
+ * Sample parameters given a range of continuous or categorical number.
  */
 abstract class ParamSampler[T](val name: String, val paramType: String) {
+
+  /**
+   * Sample an array of parameters.
+   */
   def getSamples(numVal: Int): Array[T]
 
+  /**
+   * Sample one parameter.
+   */
   def getOneRandomSample: T
 }
 
@@ -28,6 +52,11 @@ class IntParamSampler(
   }
 }
 
+/**
+ * Sample Double parameters given a range of Double values.
+ * @param scale default "log", which represents evenly sampling in a log space. Otherwise, the
+ *              sampling is performed in the linear space.
+ */
 class DoubleParamSampler(
     override val name: String,
     val minVal: Double,

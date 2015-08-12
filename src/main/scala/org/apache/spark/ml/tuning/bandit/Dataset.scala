a8780c620ea301aeccc904154c7aa22e0c3e27c8
Xusen Yin <yinxusen@gmail.com>
2015-08-12 10:21:49 +0800

    add comments in dataset

--- src/main/scala/org/apache/spark/ml/tuning/bandit/Dataset.scala
+++ src/main/scala/org/apache/spark/ml/tuning/bandit/Dataset.scala
@@ -1,20 +1,45 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package org.apache.spark.ml.tuning.bandit
 
 import org.apache.spark.ml.feature.StandardScaler
 import org.apache.spark.mllib.util.MLUtils
 import org.apache.spark.sql.{DataFrame, Row, SQLContext}
 
-/**
- * Created by panda on 8/9/15.
- */
 abstract class Dataset(
     val name: String,
     val trainingSet: DataFrame,
     val validationSet: DataFrame,
     val testSet: DataFrame) {
+
+  /**
+   * A base validation error.
+   */
   def baseValErr(): Double
 }
 
+/**
+ * Classification dataset contains training, validation, and test set. Each of them is a
+ * [DataFrame], of which the `label` column represents a `Double` target, while the `features`
+ * column is refer to features, which is a `Vector`.
+ *
+ * Note that column names cannot modify, in accord to other processing scripts.
+ */
 class ClassifyDataset(
     override val name: String,
     override val trainingSet: DataFrame,
@@ -30,6 +55,12 @@ class ClassifyDataset(
 }
 
 object ClassifyDataset {
+  /**
+   * Partitioning the given data (via file name) into training, validation, and test set, then
+   * standard scaling them according to the training set.
+   *
+   * All features would be scaled, however the scaling of label is optional.
+   */
   def scaleAndPartitionData(
       sqlCtx: SQLContext,
       dataName: String,
